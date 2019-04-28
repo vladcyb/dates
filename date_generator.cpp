382c222ba3ea9b98cfeaf6073e68a2f8eeb53ce4
12312312312 <vladtsibizov@yandex.ru>
2019-04-28 04:13:52 +0300

    correction

--- date_generator.cpp
+++ date_generator.cpp
@@ -7,17 +7,8 @@ int DateGenerator::GetRandomMonth(){
     return GetRandomInt(1, 12);
 }
 int DateGenerator::GetRandomDay(int year, int month){
-    if (month == 2){
-        if (IsLeapYear(year)){
-            return 29;
-        } else {
-            return 28;
-        }
-    } else if (Date::thirtyOneDaysMonths.find(month) != Date::thirtyOneDaysMonths.end()){
-        return 31;
-    } else {
-        return 30;
-    }
+    int max_val = MaxDayInMonth(year, month);
+    return GetRandomInt(1, max_val);
 }
 int DateGenerator::GetRandomInt(int min_val, int max_val){
     size_t seed = abs(rand() * rand());
